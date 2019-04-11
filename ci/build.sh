d88d426c0840e1d39e8d294d579c163ebc2760a1
KangLin <kl222@126.com>
2019-04-11 09:39:24 +0800

    Update travis ci

--- ci/build.sh
+++ ci/build.sh
@@ -77,10 +77,15 @@ if [ -n "$GENERATORS" ]; then
          -DQt5_DIR=${QT_ROOT}/lib/cmake/Qt5
     cmake --build . --target install --config Release ${RABBIT_MAKE_JOB_PARA}
 else
-    ${QT_ROOT}/bin/qmake ${SOURCE_DIR}/LunarCalendar.pro \
-        "CONFIG+=release" \
-        PREFIX=`pwd`/install
-    $MAKE -f Makefile
+    if [ "${BUILD_TARGERT}" = "android" ]; then
+        ${QT_ROOT}/bin/qmake ${SOURCE_DIR}/LunarCalendar.pro \
+            "CONFIG+=release" 
+    else
+        ${QT_ROOT}/bin/qmake ${SOURCE_DIR}/LunarCalendar.pro \
+            "CONFIG+=release" \
+            PREFIX=`pwd`/install
+    fi
+    $MAKE
     echo "$MAKE install ...."
     $MAKE install
 fi
