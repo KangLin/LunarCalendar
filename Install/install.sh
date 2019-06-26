70e29bbc5cb354afa1e6208dc2fdecca78f9fd8c
KangLin <kl222@126.com>
2019-06-26 10:16:50 +0800

    Modify install.sh

--- Install/install.sh
+++ Install/install.sh
@@ -11,17 +11,20 @@ case "$1" in
     
     install|*)
         echo "install ..."
-        if [ ! -f /usr/share/applications/LunarCalendar.desktop ]; then
-            ln -s `pwd`/share/applications/LunarCalendar.desktop /usr/share/applications/LunarCalendar.desktop
-            sed -i "s/Exec=.*//g" /usr/share/applications/LunarCalendar.desktop
-            echo "Exec=`pwd`/LunarCalendar-x86_64.AppImage" >> /usr/share/applications/LunarCalendar.desktop            
+        if [ -f /usr/share/applications/LunarCalendar.desktop ]; then
+            rm /usr/share/applications/LunarCalendar.desktop
         fi
-        if [ ! -f /usr/share/pixmaps/calendar.png ]; then
-            if [ ! -d /usr/share/pixmaps ]; then
-                mkdir -p /usr/share/pixmaps
-            fi
-            ln -s `pwd`/share/pixmaps/LunarCalendar.png /usr/share/pixmaps/LunarCalendar.png
+       	ln -s `pwd`/share/applications/LunarCalendar.desktop /usr/share/applications/LunarCalendar.desktop
+        sed -i "s/Exec=.*//g" `pwd`/share/applications/LunarCalendar.desktop
+        echo "Exec=`pwd`/LunarCalendar-x86_64.AppImage" >> `pwd`/share/applications/LunarCalendar.desktop
+        
+        if [ -f /usr/share/pixmaps/calendar.png ]; then
+            rm /usr/share/pixmaps/calendar.png
         fi
+        if [ ! -d /usr/share/pixmaps ]; then
+            mkdir -p /usr/share/pixmaps
+        fi
+        ln -s `pwd`/share/pixmaps/LunarCalendar.png /usr/share/pixmaps/LunarCalendar.png
         ;;
         
 esac
