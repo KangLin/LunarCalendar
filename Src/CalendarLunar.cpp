3e0c143c139f5c59a0427a1f91e0c85e2de1c7d7
Kang Lin <kl222@126.com>
2023-12-04 16:00:26 +0800

    Add comments

--- Src/CalendarLunar.cpp
+++ Src/CalendarLunar.cpp
@@ -9,10 +9,14 @@
 #include <QTime>
 #include "LunarTable.h"
 
+// 天干地支: https://baike.baidu.com/item/%E5%A4%A9%E5%B9%B2%E5%9C%B0%E6%94%AF/278140
+// 天干地支纪年法: https://zhuanlan.zhihu.com/p/616094910
+
 static const QString g_Gan[] = {"甲", "乙", "丙", "丁", "戊", "己", "庚", "辛", "壬", "癸"};
 static const QString g_Zhi[] = {"子", "丑", "寅", "卯", "辰", "巳", "午", "未", "申", "酉", "戌", "亥"};
 static const QString g_ShX[] = {"鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡", "狗", "猪"};
-static const QString g_numCn[] = {"零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十"}; //中文数字
+static const QString g_numCn[] = {"零", "一", "二", "三", "四", "五",
+                                  "六", "七", "八", "九", "十"}; //中文数字
 static const QString g_jqmc[] = {"冬至", "小寒", "大寒", "立春",
                                  "雨水", "惊蛰", "春分", "清明",
                                  "谷雨", "立夏", "小满", "芒种",
@@ -26,7 +30,11 @@ static const QString g_jqImage[] = {"DongZhi", "XiaoHan", "DaHan", "LiChun",
                                     "ChuShu", "BaiLu", "Qiufeng", "HanLu",
                                     "ShungJiang", "LiDong", "XiaoXue", "DaXue"};
 static const QString g_ymc[] = {"十一", "十二", "正", "二", "三", "四", "五", "六", "七", "八", "九", "十"}; //月名称,建寅
-static const QString g_rmc[] = {"初一", "初二", "初三", "初四", "初五", "初六", "初七", "初八", "初九", "初十", "十一", "十二", "十三", "十四", "十五", "十六", "十七", "十八", "十九", "廿", "廿一", "廿二", "廿三", "廿四", "廿五", "廿六", "廿七", "廿八", "廿九", "卅", "卅一"};
+static const QString g_rmc[] = {"初一", "初二", "初三", "初四", "初五", "初六",
+                                "初七", "初八", "初九", "初十", "十一", "十二",
+                                "十三", "十四", "十五", "十六", "十七", "十八",
+                                "十九", "廿", "廿一", "廿二", "廿三", "廿四",
+                                "廿五", "廿六", "廿七", "廿八", "廿九", "卅", "卅一"};
 
 static QMap<int, QMap<int, QStringList> > g_Holiday;     //假日
 static QMap<int, QMap<int, QStringList> > g_Anniversary; //周年
