8adf94de16afd105e437a24f6ddc4ca9fab5d23c
Kang Lin <kl222@126.com>
2023-12-14 15:09:59 +0800

    Release v0.3.1

--- ci/build.sh
+++ ci/build.sh
@@ -109,10 +109,10 @@ case ${BUILD_TARGERT} in
 esac
 
 if [ -n "$appveyor_build_version" -a -z "$VERSION" ]; then
-    export VERSION="v0.3.0"
+    export VERSION="v0.3.1"
 fi
 if [ -z "$VERSION" ]; then
-    export VERSION="v0.3.0"
+    export VERSION="v0.3.1"
 fi
 export UPLOADTOOL_BODY="Release LunarCalendar-${VERSION}.<br> The change see [ChangeLog.md](ChangeLog.md)"
 #export UPLOADTOOL_PR_BODY=
