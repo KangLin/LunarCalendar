050fa61385b8d758a4d4898c609aa56a8d6b79b5
Kang Lin <kl222@126.com>
2024-05-31 11:58:09 +0800

    Modfiy rules and CMakeLists.
    If install RabbitCommon develp kits. the install to /usr
    If use RabbitCommon source code. the install to /opt/LunarCalendar

--- deploy.sh
+++ deploy.sh
@@ -4,23 +4,37 @@ set -e
 
 SOURCE_DIR=`pwd`
 
+PRE_TAG=`git tag --sort=-creatordate | head -n 1`
+
 if [ -n "$1" ]; then
     VERSION=`git describe --tags`
     if [ -z "$VERSION" ]; then
-        VERSION=` git rev-parse HEAD`
+        VERSION=`git rev-parse HEAD`
     fi
-    
-    echo "Current version: $VERSION, The version to will be set: $1"
+
+    if [ -n "$2" ]; then
+        MESSAGE="Release $1 $2"
+    else
+        MESSAGE="Release $1"
+    fi
+
+    PRE_TAG=`git tag --sort=-taggerdate | head -n 1`
+    echo "Current version: $VERSION, current tag: $PRE_TAG. The version to will be set tag version: $1 message: $MESSAGE"
     echo "Please check the follow list:"
     echo "    - Test is ok ?"
     echo "    - Translation is ok ?"
     echo "    - Setup file is ok ?"
+    echo "    - Update_*.xml is ok ?"
     
     read -t 30 -p "Be sure to input Y, not input N: " INPUT
     if [ "$INPUT" != "Y" -a "$INPUT" != "y" ]; then
         exit 0
     fi
-    git tag -a $1 -m "Release $1"
+    git tag -a $1 -m "Release $1 ${MESSAGE}"
+else
+    echo "Usage: $0 release_version [release_message]"
+    echo "   release_version format: [v][0-9].[0-9].[0-9]"
+    exit -1
 fi
 
 VERSION=`git describe --tags`
@@ -42,8 +56,6 @@ EOF
 
 #mkchangelog $VERSION ChangeLog.md
 
-sed -i "s/    SET(LunarCalendar_VERSION.*/    SET(LunarCalendar_VERSION \"${VERSION}\")/g" ${SOURCE_DIR}/CMakeLists.txt
-sed -i "s/    SET(LunarCalendar_VERSION.*/    SET(LunarCalendar_VERSION \"${VERSION}\")/g" ${SOURCE_DIR}/Src/CMakeLists.txt
 sed -i "s/^\!define PRODUCT_VERSION.*/\!define PRODUCT_VERSION \"${VERSION}\"/g" ${SOURCE_DIR}/Install/Install.nsi
 sed -i "s/^version: '.*{build}'/version: '${VERSION}.{build}'/g" ${SOURCE_DIR}/appveyor.yml
 sed -i "s/^\  - export VERSION=.*/\  - export VERSION=\"${VERSION}\"/g" ${SOURCE_DIR}/.travis.yml
@@ -59,6 +71,9 @@ sed -i "s/v[0-9]\+\.[0-9]\+\.[0-9]\+/${VERSION}/g" ${SOURCE_DIR}/README*.md
 
 DEBIAN_VERSION=`echo ${VERSION}|cut -d "v" -f 2`
 #sed -i "s/lunarcalendar_[0-9]\+\.[0-9]\+\.[0-9]\+/lunarcalendar_${DEBIAN_VERSION}/g" ${SOURCE_DIR}/README*.md
+
+sed -i "s/    SET(LunarCalendar_VERSION.*/    SET(LunarCalendar_VERSION \"${DEBIAN_VERSION}\")/g" ${SOURCE_DIR}/CMakeLists.txt
+sed -i "s/    SET(LunarCalendar_VERSION.*/    SET(LunarCalendar_VERSION \"${DEBIAN_VERSION}\")/g" ${SOURCE_DIR}/Src/CMakeLists.txt
 
 sed -i "s/lunarcalendar (.*)/lunarcalendar (${DEBIAN_VERSION})/g" ${SOURCE_DIR}/debian/changelog
 sed -i "s/[0-9]\+\.[0-9]\+\.[0-9]\+/${DEBIAN_VERSION}/g" ${SOURCE_DIR}/App/android/AndroidManifest.xml
